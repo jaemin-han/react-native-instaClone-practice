4c5ede7cec09fe5da3f7bda86227e23d51dbbd39
jaemin.han <jaemin.han@nyu.edu>
2018-04-28 16:18:28 -0400

    image and style setup complete

--- App.js
+++ App.js
@@ -4,19 +4,6 @@ import InstaClone from './src/InstaClone.js';
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <InstaClone />
-      </View>
-    );
+    return <InstaClone />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
--- src/InstaClone.js
+++ src/InstaClone.js
@@ -4,11 +4,35 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 class InstaClone extends Component {
     render(){
         return(
-            <View>
-                <Text>My InstaClone Starts Here</Text>
+            <View style={{ flex: 1, width: 100 + '%', height: 100 + '%'}}>
+                <View style={styles.tempNav}>
+                    <Text>Instagramini</Text>
+                </View>
+                
+               
+ 
+                <Image 
+                    style={{ width: 100+'%', height: 250}}
+                    source={{
+                        uri: "https://lh3.googleusercontent.com/mUJK_0Vvc20XsMsZEPTjWZkR24EDls8ViSy3ITn6laO5O2U4efG7EYcHoHd9jb1lV0i_NFZF5AkCHFjt5PFkOWSVmBU"
+                    }} />
             </View>
         )
     }
 }
+
+const styles= StyleSheet.create({
+    tempNav: {
+        width: 100 + '%',
+        height: 65,
+        marginTop: 30,
+        backgroundColor:'rgb(250, 250, 250)',
+        borderBottomColor: 'rgb(233, 233, 233)',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        justifyContent: 'center',
+        alignItems: "center"
+    }
+
+})
 
 export default InstaClone;
