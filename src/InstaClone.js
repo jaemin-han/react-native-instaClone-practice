c5c2738ea9c804c9a7f4cfb4e6c28657292f23a8
jaemin.han <jaemin.han@nyu.edu>
2018-04-29 23:46:29 -0400

    adjust profile section and image size adjustment with js style class

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,38 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 
 class InstaClone extends Component {
+    constructor(){
+        super()
+        this.state = {
+            screenWidth: Dimensions.get("window").width
+        };
+    }
+
     render(){
+        const imageUri = "https://lh3.googleusercontent.com/mUJK_0Vvc20XsMsZEPTjWZkR24EDls8ViSy3ITn6laO5O2U4efG7EYcHoHd9jb1lV0i_NFZF5AkCHFjt5PFkOWSVmBU" + "=s" + this.state.screenWidth;
         return(
             <View style={{ flex: 1, width: 100 + '%', height: 100 + '%'}}>
                 <View style={styles.tempNav}>
                     <Text>Instagramini</Text>
                 </View>
                 <View style={styles.userBar}>
-                    <View style={{ flexDirection: "row" }}>
+                    <View style={{ flexDirection: "row", alignItems: "center" }}>
                         <Image 
-                            style={{width: 40, height: 40 }}
+                            style={styles.userPic}
                             source={{
                                 uri:
                                     "https://lh3.googleusercontent.com/jGip3ceNulaMJu0b2TT6IQwYCKNQIcSOCtptpVsYnsssFtfPF0AVGRlIXfpF6JVyOIwwjzaSSAhH11oJQAJOKEt5Lw"
                             }}
                         />
-                        <Text>Jaemin Han</Text>
-                    
+                        <Text style={{ marginLeft: 10 }}>Jaemin Han</Text>
                     </View>
-                    <View />
+                    <View style={{ alignItems: "center" }}>
+                        <Text style={{ fontSize: 30 }}>...</Text>
+                    </View>
                 </View>
 
-
-          
-                
-                
-               
- 
                 <Image 
-                    style={{ width: 100+'%', height: 250}}
+                    style={{ width: this.state.screenWidth, height: 400}}
                     source={{
-                        uri: "https://lh3.googleusercontent.com/mUJK_0Vvc20XsMsZEPTjWZkR24EDls8ViSy3ITn6laO5O2U4efG7EYcHoHd9jb1lV0i_NFZF5AkCHFjt5PFkOWSVmBU"
+                        uri: imageUri
                     }} />
             </View>
         )
@@ -55,7 +58,16 @@ const styles= StyleSheet.create({
         width: 100 + "%",
         height: 50,
         backgroundColor: "rgb(255, 255, 255)",
-        flexDirection: "row"
+        flexDirection: "row",
+        paddingHorizontal: 10,
+        // flexDirection: 'row',
+        justifyContent: "space-between"
+    },
+
+    userPic: {
+        height: 40,
+        width: 40,
+        borderRadius: 20
     }
 
 })
